431f5a581dd523c11ba80ba9d38afa5fc52575a3
AazimAnish <aazimanish1@gmail.com>
2024-03-23 09:18:50 +0530

    feat: AA

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import {
+  ThirdwebProvider,
+  ConnectWallet,
+  localWallet,
+  embeddedWallet,
+} from "@thirdweb-dev/react";
 
 function App() {
   return (
-    <div className='bg-black'>
-      hihi
+    <div>
+      <ThirdwebProvider
+      activeChain="sepolia"
+      clientId="cdd8254e7fc70530f20b83272500e204"
+      // locale={en()}
+      supportedWallets={[
+        localWallet(),
+        embeddedWallet({
+          auth: {
+            options: [
+              "email",
+              "facebook",
+              "apple",
+              "google",
+            ],
+          },
+        }),
+      ]}
+    >
+      <ConnectWallet
+        theme={"dark"}
+        modalSize={"wide"}
+      />
+    </ThirdwebProvider>
     </div>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  ThirdwebProvider,
+  metamaskWallet,
+  coinbaseWallet,
+  walletConnect,
+} from "@thirdweb-dev/react";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ThirdwebProvider
+      supportedWallets={[
+        metamaskWallet({
+          recommended: true,
+        }),
+        coinbaseWallet(),
+        walletConnect(),
+      ]}
+      clientId="<your_client_id>"
+    >
     <App />
+    </ThirdwebProvider>
   </React.StrictMode>,
 )
