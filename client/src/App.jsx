6656b5b16536f8f3a1ec65ed2d69f58af280dbac
aakash <aakashpr975@gmail.com>
2024-03-23 13:55:55 +0530

    bg-color

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import BrowserRouter, Routes, and Route
-import ChatPage from './pages/ChatPage';
-import Login from './pages/Login';
-import Displaycards from './pages/Displaycards';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import BrowserRouter, Routes, and Route
+import ChatPage from "./pages/ChatPage";
+import Login from "./pages/Login";
+import Displaycards from "./pages/Displaycards";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
-      <div className=''>
+      <div className="bg-gray-900">
+        <Header /> {/* Header component */}
         {/* Define routes for different pages */}
         <Routes>
-          <Route path="/" element={<Login />} /> 
-          <Route path="/chat" element={<ChatPage />} /> {/* Route for ChatPage */}
-          <Route path="/displaycards" element={<Displaycards />} /> {/* Route for Displaycards page */}
+          <Route path="/" element={<Login />} />
+          <Route path="/chat" element={<ChatPage />} />{" "}
+          {/* Route for ChatPage */}
+          <Route path="/displaycards" element={<Displaycards />} />{" "}
+          {/* Route for Displaycards page */}
         </Routes>
       </div>
     </Router>
