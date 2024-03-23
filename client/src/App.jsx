f08f113ee04ad91205ea8c42b7ab057c4cee6528
aiswarya s <aiswarya.s4528@gmail.com>
2024-03-23 10:04:22 +0530

    card component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 // import Chat from './components/Chat'
 import ChatPage from './pages/ChatPage'
 import Login from './pages/Login'
+import Displaycards from './pages/Displaycards'
 
 function App() {
   return (
     <div className=''>
       <ChatPage/>
       <Login/>
+      <Displaycards/>
+
     </div>
   )
 }
---
+++ client/src/pages/Displaycards.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from '../components/Card'
+
+const Displaycards = () => {
+  return (
+    <div>
+      <h1 classname="font-epilogue font-semibold text-[18px] text-white text-left">All Items</h1>
+    <Card/>
+    </div>
+  )
+}
+
+export default Displaycards
