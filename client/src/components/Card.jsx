6428bb35d23044c2b990363574dc6e158b7d6ff0
aiswarya s <aiswarya.s4528@gmail.com>
2024-03-23 16:33:06 +0530

    display page modified

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,38 +1,35 @@
 import React from 'react';
-import Books from '../assets/Books.jpg';
 import tagType from '../assets/type.svg';
 import plus from '../assets/plus.svg';
 import minus from '../assets/minus.svg';
 
-const Card = () => {
+const Card = ({ image, category, amount }) => {
   return (
     <div className="sm:w-[288px] w-full rounded-[15px] bg-[#1c1c24] cursor-pointer">
-      <img src={Books} alt="Books" className="w-full h-[158px] object-cover rounded-[15px]"/>
+      <img src={image} alt={category} className="w-full h-[158px] object-cover rounded-[15px]"/>
     
-    <div className="flex flex-col p-4">
-      <div className="flex flex-row items-center justify-between mb-[18px] ">
-        <div className="flex items-center">
-        <img src={tagType} alt="tag" className="w-[17px] h-[17px] object-contain"/>
-        <p className="ml-[12px] mt-[2px] font-epilogue font-medium text-[12px] text-[#808191]">Category</p>
-        </div>
-        <div className="flex items-center">
-        <img src={plus} alt="plus" className="w-[17px] h-[17px] filter invert mr-3"/>
-        <img src={minus} alt="plus" className="w-[17px] h-[17px] filter invert"/>
+      <div className="flex flex-col p-4">
+        <div className="flex flex-row items-center justify-between mb-[18px] ">
+          <div className="flex items-center">
+            <img src={tagType} alt="tag" className="w-[17px] h-[17px] object-contain"/>
+            <p className="ml-[12px] mt-[2px] font-epilogue font-medium text-[12px] text-[#808191]">{category}</p>
+          </div>
+          <div className="flex items-center">
+            <img src={plus} alt="plus" className="w-[17px] h-[17px] filter invert mr-3"/>
+            <img src={minus} alt="plus" className="w-[17px] h-[17px] filter invert"/>
+          </div>
         </div>
         
-      </div>
-      
-      <div className="flex justify-between items-center mb-2">
-      <div className="block">
-        <h3 className="font-epilogue font-semibold text-[16px] text-white text-left leading-[26px] truncate">Total Amount:</h3>
-        <p className ="mt-[5px] font-epilogue font-normal font-[12px] text-[#808191] text-left leading-[18px] truncate">10000</p>
-
-      </div>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 rounded-xl">
+        <div className="flex justify-between items-center mb-2">
+          <div className="block">
+            <h3 className="font-epilogue font-semibold text-[16px] text-white text-left leading-[26px] truncate">Total Amount:</h3>
+            <p className="mt-[5px] font-epilogue font-normal font-[12px] text-[#808191] text-left leading-[18px] truncate">{amount}</p>
+          </div>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 rounded-xl">
             Submit
           </button>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
--- client/src/pages/Displaycards.jsx
+++ client/src/pages/Displaycards.jsx
@@ -1,13 +1,62 @@
-import React from 'react'
-import Card from '../components/Card'
+import React from 'react';
+import Card from '../components/Card';
+import search from '../assets/search.svg';
+import Books from '../assets/Books.jpg';
+import Clothes from '../assets/clothes.jpg';
+import Rice from '../assets/rice.jpg';
+import Stationary from '../assets/stationary.jpg';
+import Wheat from '../assets/wheatsack.jpg';
 
 const Displaycards = () => {
+  const cardsData = [ 
+    {
+      id: 1,
+      image: Books,
+      amount: 10000,
+      category: 'Book'
+    },
+    {
+      id: 2,
+      image: Clothes,
+      amount: 20000,
+      category: 'Clothes'
+    },
+    {
+      id: 3,
+      image: Rice,
+      amount: 8000,
+      category: 'Rice'
+    },
+    {
+      id: 4,
+      image: Stationary,
+      amount: 12000,
+      category: 'Stationary'
+    },
+    {
+      id: 5,
+      image: Wheat,
+      amount: 12000,
+      category: 'Wheat'
+    }
+  ];
+
   return (
-    <div>
-      <h1 classname="font-epilogue font-semibold text-[18px] text-white text-left">All Items</h1>
-    <Card/>
+    <div className="pt-4">
+      <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]" >
+        <input type="text" placeholder="search for product" className="flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none"/>
+        <div className="w-[72px] h-full rounded-[20px] bg-[#4acd8d] flex justify-center items-center cursor-pointer">
+          <img src={search} alt="search" className="w-[15px] h-[15px] object-contain filter invert"/>
+        </div>
+      </div>
+    
+      <div className="sm:flex hidden flex-row justify-start gap-5 pt-[4rem]">
+        {cardsData.map((card, index) => (
+          <Card key={card.id} image={card.image} amount={card.amount} category={card.category} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Displaycards
+export default Displaycards;
