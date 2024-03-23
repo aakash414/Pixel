293e26bcec432dafd53ca9790634582bc45708ab
aakash <aakashpr975@gmail.com>
2024-03-23 10:55:31 +0530

    hardhat config

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ChainId, ThirdwebProvider } from "thirdweb-dev/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ThirdwebProvider desiredChainId={ChainId.Sepolia}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    ,
+  </ThirdwebProvider>
+);
