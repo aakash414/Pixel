2f9d4bdc75069625d49011d50b950d5ff387db26
aakash <aakashpr975@gmail.com>
2024-03-24 06:20:55 +0530

    bug fix profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,23 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { useStateContext } from "../../web3_client/web3_logic.jsx";
-import DisplayCards from "./DisplayCards.jsx";
-
 const Profile = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [campaigns, setCampaigns] = useState([]);
-
-  const { address, contract, getUserCampaigns } = useStateContext();
-
-  const fetchCampaigns = async () => {
-    setIsLoading(true);
-    const data = await getUserCampaigns();
-    setCampaigns(data);
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    if (contract) fetchCampaigns();
-  }, [address, contract]);
+  const walletAddress = localStorage.getItem("walletAddress");
 
   return (
     <div className="w-full h-auto bg-gray-900">
