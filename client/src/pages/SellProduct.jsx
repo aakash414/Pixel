9aeae016d049bb7fe07c1b3b08e48f8477934b26
aakash <aakashpr975@gmail.com>
2024-03-24 06:29:36 +0530

    sell page

--- client/src/pages/SellProduct.jsx
+++ client/src/pages/SellProduct.jsx
@@ -1,30 +1,30 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-import { ethers } from 'ethers';
-import loader from '../assets/loader.svg';
-import InputForm from '../components/InputForm';
-import { useStateContext } from '../../web3_client/web3_logic';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ethers } from "ethers";
+import loader from "../assets/loader.svg";
+import InputForm from "../components/InputForm";
+import { useStateContext } from "../../web3_client/web3_logic";
 
 const SellProduct = () => {
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
   const { createCampaign } = useStateContext();
   const [form, setForm] = useState({
-    name: '',
-    title: '',
-    description: '',
-    target: '', 
-    deadline: '',
-    image: ''
+    name: "",
+    title: "",
+    description: "",
+    target: "",
+    deadline: "",
+    image: "",
   });
 
   const handleFormFieldChange = (fieldName, e) => {
-    setForm({ ...form, [fieldName]: e.target.value })
-  }
+    setForm({ ...form, [fieldName]: e.target.value });
+  };
 
   const checkIfImage = (url, callback) => {
     fetch(url)
-      .then(res => {
+      .then((res) => {
         if (res.status === 200) {
           callback(true);
         } else {
@@ -45,98 +45,106 @@ const SellProduct = () => {
       >
         {title}
       </button>
-    )
+    );
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     checkIfImage(form.image, async (exists) => {
-      if(exists) {
-        setIsLoading(true)
-        await createCampaign({ ...form, target: ethers.utils.parseUnits(form.target, 18)})
+      if (exists) {
+        setIsLoading(true);
+        await createCampaign({
+          ...form,
+          target: ethers.utils.parseUnits(form.target, 18),
+        });
         setIsLoading(false);
-        navigate('/');
+        navigate("/");
       } else {
-        alert('Provide valid image URL')
-        setForm({ ...form, image: '' });
+        alert("Provide valid image URL");
+        setForm({ ...form, image: "" });
       }
-    })
-  }
+    });
+  };
 
   return (
-    <div className="bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
+    <div className="bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4 ">
       {isLoading && (
-      <div>
-        <img src={loader} alt="Loading..." />
-        <p>Transaction is in progress. Please wait...</p>
-      </div>
+        <div>
+          <img src={loader} alt="Loading..." />
+          <p>Transaction is in progress. Please wait...</p>
+        </div>
       )}
       <div className="flex justify-center items-center p-[16px] sm:min-w-[380px] bg-[#3a3a43] rounded-[10px]">
-        <h1 className="font-epilogue font-bold sm:text-[25px] text-[18px] leading-[38px] text-white">Start a Campaign</h1>
+        <h1 className="font-epilogue font-bold sm:text-[25px] text-[18px] leading-[38px] text-white">
+          Sell Product
+        </h1>
       </div>
 
-      <form onSubmit={handleSubmit} className="w-full mt-[65px] flex flex-col gap-[30px]">
+      <form
+        onSubmit={handleSubmit}
+        className="w-full mt-[65px] flex flex-col gap-[30px]"
+      >
         <div className="flex flex-wrap gap-[40px]">
-          <InputForm 
+          <InputForm
             labelName="Your Name *"
             placeholder="John Doe"
             inputType="text"
             value={form.name}
-            handleChange={(e) => handleFormFieldChange('name', e)}
+            handleChange={(e) => handleFormFieldChange("name", e)}
           />
-          <InputForm 
-            labelName="Campaign Title *"
+          <InputForm
+            labelName="Product Title *"
             placeholder="Write a title"
             inputType="text"
             value={form.title}
-            handleChange={(e) => handleFormFieldChange('title', e)}
+            handleChange={(e) => handleFormFieldChange("title", e)}
           />
         </div>
 
-        <InputForm 
-            labelName="Story *"
-            placeholder="Write your story"
-            isTextArea
-            value={form.description}
-            handleChange={(e) => handleFormFieldChange('description', e)}
-          />
+        <InputForm
+          labelName="Details *"
+          placeholder="Enter details here"
+          isTextArea
+          value={form.description}
+          handleChange={(e) => handleFormFieldChange("description", e)}
+        />
 
         <div className="flex flex-wrap gap-[40px]">
-          <InputForm 
-            labelName="Goal *"
+          <InputForm
+            labelName="Total Amount *"
             placeholder="ETH 0.50"
             inputType="text"
             value={form.target}
-            handleChange={(e) => handleFormFieldChange('target', e)}
+            handleChange={(e) => handleFormFieldChange("target", e)}
           />
-          <InputForm 
+          <InputForm
             labelName="End Date *"
             placeholder="End Date"
             inputType="date"
             value={form.deadline}
-            handleChange={(e) => handleFormFieldChange('deadline', e)}
+            handleChange={(e) => handleFormFieldChange("deadline", e)}
           />
         </div>
 
-        <InputForm 
-            labelName="Campaign image *"
-            placeholder="Place image URL of your campaign"
-            inputType="url"
-            value={form.image}
-            handleChange={(e) => handleFormFieldChange('image', e)}
-          />
+        <InputForm
+          labelName="Product image *"
+          placeholder="Place image URL of your Product"
+          inputType="url"
+          value={form.image}
+          handleChange={(e) => handleFormFieldChange("image", e)}
+        />
 
-          <div className="flex justify-center items-center mt-[40px]">
-            <CustomButton 
-              btnType="submit"
-              title="Submit new campaign"
-              styles="bg-[#1dc071]"
-            />
-          </div>
+        <div className="flex justify-center items-center mt-[40px]">
+          <CustomButton
+            btnType="submit"
+            title="Submit new Product"
+            styles="bg-[#9333ea]"
+          />
+        </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default SellProduct
+export default SellProduct;
